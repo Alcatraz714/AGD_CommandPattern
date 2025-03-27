8da963c976541760edd43c6baf7fd525de54f5ff
Rishi Saxena <rishi.saxena@gamestacy.com>
2025-03-27 14:00:02 +0530

    Updated Actions to Commands

--- Assets/Scripts/Command/Abstract Commands/UnitCommand.cs
+++ Assets/Scripts/Command/Abstract Commands/UnitCommand.cs
@@ -7,6 +7,14 @@ public abstract class UnitCommand : ICommand
     public int ActorPlayerID;
     public int TargetPlayerID;
 
+    public CommandData(int ActorUnitID, int TargetUnitID,int ActorPlayerID, int TargetPlayerID)
+        {
+            this.ActorUnitID = ActorUnitID;
+            this.TargetUnitID = TargetUnitID;
+            this.ActorPlayerID = ActorPlayerID;
+            this.TargetPlayerID = TargetPlayerID;
+        }
+
     protected UnitController actorUnit;
     protected UnitController targetUnit;
 
