948ed79d9d3cf34b3672b88dfc81cbb779fa7159
Rishi Saxena <rishi.saxena@gamestacy.com>
2025-03-30 14:27:04 +0530

    Added Undo and its implementations to all commands

--- Assets/Scripts/Command/Concrete Commands/AttackStanceCommand.cs
+++ Assets/Scripts/Command/Concrete Commands/AttackStanceCommand.cs
@@ -14,6 +14,14 @@ namespace Command.Commands
         }
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.AttackStance).PerformAction(actorUnit, targetUnit, willHitTarget);
+        public override void Undo()
+        {
+            if(willHitTarget)
+            {
+                targetUnit.CurrentPower -= (int)(targetUnit.CurrentPower * 0.2f); // reduce the power increase since we are no longer in attack stance
+                actorUnit.Owner.ResetCurrentActiveUnit();
+            }
+        }
 
         public override bool WillHitTarget() => true;
     }
--- Assets/Scripts/Command/Concrete Commands/ThirdEyeCommand.cs
+++ Assets/Scripts/Command/Concrete Commands/ThirdEyeCommand.cs
@@ -5,6 +5,7 @@ namespace Command.Commands
     public class ThirdEyeCommand : UnitCommand
     {
         private bool willHitTarget;
+        private int previousHealth;
 
         public ThirdEyeCommand(CommandData commandData)
         {
@@ -12,7 +13,23 @@ namespace Command.Commands
             willHitTarget = WillHitTarget();
         }
 
-        public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.ThirdEye).PerformAction(actorUnit, targetUnit, willHitTarget);
+        public override void Execute()
+        {
+            previousHealth = targetUnit.CurrentHealth;
+            GameService.Instance.ActionService.GetActionByType(CommandType.ThirdEye).PerformAction(actorUnit, targetUnit, willHitTarget);
+        }
+
+        public override void Undo()
+        {
+            if(!targetUnit.IsAlive())
+                targetUnit.Revive();
+            // we restore the damage done based on target health
+            int healthToRestore = (int)(previousHealth * 0.25f);
+            targetUnit.RestoreHealth(healthToRestore);
+            // we then reduce the extra power as well which was a side effect of this action
+            targetUnit.CurrentPower -= healthToRestore;
+            actorUnit.Owner.ResetCurrentActiveUnit();
+        }
 
         public override bool WillHitTarget() => true;
     } 
